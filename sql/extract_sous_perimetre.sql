de000f3fbb3433bca54cf1eb6ab70741b33b72fa
Ju3465 <julian.vignal@gmail.com>
2026-01-12 22:31:53 +0100

    Mise en page commentaires

--- sql/extract_sous_perimetre.sql
+++ sql/extract_sous_perimetre.sql
@@ -4,7 +4,8 @@
 SELECT
     oi.order_id, oi.id AS order_item_id, oi.created_at AS item_created_at,      -- remise des colonnes dans l'ordre du csv d'origine en enlevant les colonnes shipped_at, delivered_at et product_id qui ne sont pas indispensables
     oi.status AS item_status, oi.sale_price, p.cost, p.category, p.department, p.brand,   -- on garde order_status pour le calcul du taux de ré-achat
-    p.name AS product_name, o.status AS order_status, o.created_at AS order_created_at, u.id AS user_id, u.gender, u.country, u.state, u.city
+    p.name AS product_name, o.status AS order_status, o.created_at AS order_created_at,
+    u.id AS user_id, u.gender, u.country, u.state, u.city
 FROM `bigquery-public-data.thelook_ecommerce.order_items` oi       -- jointures à faire avec les tables orders, users, products
 INNER JOIN `bigquery-public-data.thelook_ecommerce.orders` o
   ON oi.order_id = o.order_id
